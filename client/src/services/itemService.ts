a505fcc9f8aa6e55b64d2f461fcca9b4e18c8b07
joyrahulfurtado2 <jrahul2@outlook.com>
2022-07-09 17:21:24 +0530

    react lab solution

--- client/src/services/itemService.ts
+++ client/src/services/itemService.ts
@@ -1,31 +1,24 @@
-import axios from 'axios';
-import Item from '../models/Items';
+import axios from "axios";
+import Item from "../models/Items";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 // function to get items from the backend
 const getItems = async () => {
-    const response = await axios.get<Item[]>( `${baseUrl}/items` );
-    return response.data;
+  const response = await axios.get<Item[]>(`${baseUrl}/items`);
+  return response.data;
 };
 
 // function to post items from the backend
 
-const postItem = async ( item : Omit<Item, 'id'> ) => {
-    const response = await axios.post<Item>(
-        `${baseUrl}/items`,
-        item,
-        {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-    );
+const postItem = async (item: Omit<Item, "id">) => {
+  const response = await axios.post<Item>(`${baseUrl}/items`, item, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    return response.data;
+  return response.data;
 };
 
-export {
-    getItems,
-    postItem
-};
+export { getItems, postItem };
